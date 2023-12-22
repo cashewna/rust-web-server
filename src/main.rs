2b97d2f6a7eb9cde6bd3557d1beb2963214fb470
Krishna Khong <krishna@khong.dev>
2023-12-22 19:00:12 +0800

    style: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,13 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     // Method Request-URI HTTP-Version CRLF
-    let request_line = buf_reader
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) =
-        if request_line == "GET / HTTP/1.1" {
-            ("HTTP/1.1 200 OK", "hello.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND", "404.html")
-        };
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
